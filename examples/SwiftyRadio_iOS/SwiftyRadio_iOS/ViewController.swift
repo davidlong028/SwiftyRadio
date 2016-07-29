1f5d850d392650d3054d8cf6925643182f976325
Eric Conner <ericconner@Erics-iMac.local>
2016-07-29 12:16:08 -0400

    Improved example code

--- examples/SwiftyRadio_iOS/SwiftyRadio_iOS/ViewController.swift
+++ examples/SwiftyRadio_iOS/SwiftyRadio_iOS/ViewController.swift
@@ -9,7 +9,17 @@
 import UIKit
 
 class ViewController: UIViewController {
+	@IBOutlet weak var songTitle: UILabel!
+	@IBOutlet weak var artistTitle: UILabel!
 
+	@IBAction func playPauseButton(sender: AnyObject) {
+		if swiftyRadio.isPlaying() {
+			swiftyRadio.pause()
+		} else {
+			swiftyRadio.play()
+		}
+	}
+	
 	override func viewDidLoad() {
 		super.viewDidLoad()
 		// Do any additional setup after loading the view, typically from a nib.
@@ -17,13 +27,29 @@ class ViewController: UIViewController {
 		// Setup the station
 		swiftyRadio.setStation("WTSQ 88.1FM", URL: "http://stream.wtsq.org:8000/xstream2")
 		
-		// Start playing the station
-		swiftyRadio.play()
+		NSNotificationCenter.defaultCenter().addObserver(self, selector: #selector(updateView), name: "SwiftyRadioMetadataUpdated", object: nil)
+		NSNotificationCenter.defaultCenter().addObserver(self, selector: #selector(playWasPressed), name: "SwiftyRadioPlayWasPressed", object: nil)
+		NSNotificationCenter.defaultCenter().addObserver(self, selector: #selector(pauseWasPressed), name: "SwiftyRadioPauseWasPressed", object: nil)
+		
+		swiftyRadio.customMetadata("Press Play to Begin")
 	}
 
 	override func didReceiveMemoryWarning() {
 		super.didReceiveMemoryWarning()
 		// Dispose of any resources that can be recreated.
+	}
+	
+	func updateView() {
+		songTitle.text = swiftyRadio.trackTitle()
+		artistTitle.text = swiftyRadio.trackArtist()
+	}
+	
+	func playWasPressed() {
+		swiftyRadio.customMetadata("Loading...")
+	}
+	
+	func pauseWasPressed() {
+		swiftyRadio.customMetadata("Paused...")
 	}
 
 }
